8dd3ba2c40182f8d82ce052f9388dcceb3f7dafc
Alan Nunez <nunear@icloud.com>
2018-07-14 08:01:22 -0400

    Defining Item and ItemStore class to hold  the data

---
+++ Homepwner/ItemStore.swift
@@ -0,0 +1,13 @@
+//
+//  ItemStore.swift
+//  Homepwner
+//
+//  Created by Alan Nunez on 7/14/18.
+//  Copyright © 2018 Alan Nunez. All rights reserved.
+//
+
+import UIKit
+
+class ItemStore {
+    var allItems  = [Item]()
+}
